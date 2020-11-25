b85c6533b3f2303eae355ab2d5cc5e341fe9196d
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-11-25 21:33:17 +0300

    test v1.0

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
 
 include_once 'functions.php';
+$session=$_POST['sessionId'];
+$serviceCode=$_POST['serviceCode'];
+$phoneNumber=$_POST['phoneNumber'];
+$input=$_POST['input'];
 
+if($input==""){
+     $response="CON What would you want to check \n";
+     $response.="1. Account Info\n";
+     $response.="2. Phone number";
+}elseif($input=="1"){
+     $response="CON Choose an option you want to view \n";
+     $response.="1. Account No\n";
+     $response.="2. Account Balance";
+}elseif($input=="2"){
+     $response="END Your phone number is ".$phoneNumber;
+}elseif($input=="1*1"){
+     $accNo="AC1101";
+     $response="END Your account number is ".$accNO;
+}elseif($input=="1*2"){
+     $balance="KES 1.00";
+     $response="END Your balance is ".$balance;
+}
+
+header('Content-type:text/plain');
+echo $response;
 
 ?>
