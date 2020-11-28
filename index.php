088169b5ee71b5c20a5f6d2ac5feb27b0947a397
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-11-28 12:34:57 +0300

    array

--- index.php
+++ index.php
@@ -6,20 +6,23 @@ $serviceCode=$_POST['serviceCode'];
 $phoneNumber=$_POST['phoneNumber'];
 $text=$_POST['text'];
 
-if($text==""){
+$textArray=explode('*',$text);
+$ArrayLen=count($textArray);
+
+if(empty($textArray)){
      $response="CON What would you want to check \n";
      $response.="1. Account Info\n";
      $response.="2. Phone number";
-}elseif($text=="1"){
-     $response="CON Choose an option you want to view \n";
+}elseif($textArray[0]=="1"){
+     $response="CON Choose an option you want to view\n";
      $response.="1. Account No\n";
      $response.="2. Account Balance";
-}elseif($text=="2"){
+}elseif($textArray[0]=="2"){
      $response="END Your phone number is ".$phoneNumber;
-}elseif($text=="1*1"){
+}elseif($textArray[1]=="1"){
      $accNo="AC1101";
      $response="END Your account number is ".$accNo;
-}elseif($text=="1*2"){
+}elseif($textArray[1]=="2"){
      $balance="KES 1.00";
      $response="END Your balance is ".$balance;
 }
