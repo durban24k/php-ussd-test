5e121231444b003f05fb8d72bd4f10f348d47ed3
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-12-07 20:28:33 +0300

    v2.2

--- index.php
+++ index.php
@@ -5,33 +5,32 @@
      $phonenumber=$_POST['MSISDN'];
      $serviceCode=$_POST['serviceCode'];
 
-     if (isset($text)) {
-          $level = explode("*", $text);
-          if ($text=="") {
-               $response="CON Welcome to the registration portal.\nPlease enter you full name";
-          }
-          if(isset($level[0]) && $level[0]!="" && !isset($level[1])){
-               $response="CON Hi ".$level[0].", enter your ward name";   
-          }
-          else if(isset($level[1]) && $level[1]!="" && !isset($level[2])){
-                    $response="CON Please enter you national ID number\n"; 
-          }
-          else if(isset($level[2]) && $level[2]!="" && !isset($level[3])){
-               //Save data to database
-               // $data=array(
-               //      'phonenumber'=>$phonenumber,
-               //      'fullname' =>$level[0],
-               //      'electoral_ward' => $level[1],
-               //      'national_id'=>$level[2]
-               //      );
-               $response="END Thank you ".$level[0]." for registering.\nWe will keep you updated";
-               $response.=$level[1];
-               $response.=$level[2]; 
-          }
+     $level = explode("*", $text);
+     if ($text=="") {
+          $response="CON Welcome to the registration portal.\nPlease enter you full name";
+     }
+     if(isset($level[0]) && $level[0]!="" && !isset($level[1])){
+          $response="CON Hi ".$level[0].", enter your ward name";   
+     }
+     else if(isset($level[1]) && $level[1]!="" && !isset($level[2])){
+               $response="CON Please enter you national ID number\n"; 
+     }
+     else if(isset($level[2]) && $level[2]!="" && !isset($level[3])){
+          //Save data to database
+          // $data=array(
+          //      'phonenumber'=>$phonenumber,
+          //      'fullname' =>$level[0],
+          //      'electoral_ward' => $level[1],
+          //      'national_id'=>$level[2]
+          //      );
+          $response="END Thank you ".$level[0]." for registering.\nWe will keep you updated";
+          $response.=$level[1];
+          $response.=$level[2]; 
+     }
 
      header('Content-type: text/plain');
      echo $response;
 
-     }
+
 
 ?>
